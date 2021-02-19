fafb0b5a7506a5f60dec1137e5954634ecff404a
vividswan <vividswan@naver.com>
2021-02-19 09:37:31 +0900

    feat. Jwt 라이브러리 추가 후 Jwt provider, filter 생성

---
+++ Back-End/src/main/java/com/soc/BackEnd/config/security/JwtTokenProvider.java
@@ -0,0 +1,71 @@
+package com.soc.BackEnd.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private final UserDetailsService userDetailsService;
+
+    @Value("jwt.secret")
+    private String secretKey;
+
+    private long tokenValidity = 60 * 60 * 1000L;
+
+    @PostConstruct
+    protected void init(){
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String studentId, List<String> roles){
+        Claims claims = Jwts.claims().setSubject(studentId);
+        claims.put("roles",roles);
+        Date now = new Date();
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime()  + tokenValidity))
+                .signWith(SignatureAlgorithm.ES256, secretKey)
+                .compact();
+    }
+
+    public String getStudentId(String token){
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public Authentication getAuthentication(String token){
+        UserDetails userDetails = userDetailsService.loadUserByUsername(this.getStudentId(token));
+        return  new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+    }
+
+    public String resolveToken(HttpServletRequest req){
+        return req.getHeader("X-AUTH-TOKEN");
+    }
+
+    public boolean validateToken(String jwtToken){
+        try{
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
+            return !claimsJws.getBody().getExpiration().before(new Date());
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+}
