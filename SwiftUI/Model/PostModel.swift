ae2e685fa520927eb23f8fe5898b6e849826c323
Genesis2010 <dmsgktn1685@naver.com>
2021-07-15 20:05:04 +0900

    feat: 게시글 구성 데이터

---
+++ SwiftUI/Model/PostModel.swift
@@ -0,0 +1,24 @@
+//
+//  post_data.swift
+//  SOCprojectUI
+//
+//  Created by 박준혁 on 2021/07/10.
+//
+
+import SwiftUI
+
+struct PostModel: Identifiable {
+    
+//    var id = UUID().uuidString
+//    var contents: String
+//    var timing: Data
+//    var comment_cnt: Int
+    
+    var id: String
+    var contents: String
+    var timing: String
+    var comment_cnt: String
+    
+}
+
+// 닉네임, 글 내용, 올린시간, 댓글 수
