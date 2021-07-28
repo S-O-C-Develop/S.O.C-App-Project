c7c0ebdf22381f1e9cd8f0363263852915852322
vividswan <vividswan@naver.com>
2021-07-29 00:34:29 +0900

    refactor: Account 인증 코드에서 Security ContextHolder 은닉

--- Back-End/src/main/java/com/soc/BackEnd/account/AccountController.java
+++ Back-End/src/main/java/com/soc/BackEnd/account/AccountController.java
@@ -6,17 +6,15 @@ import com.soc.BackEnd.account.dto.ResponseAccountDto;
 import com.soc.BackEnd.account.dto.SignupDto;
 import com.soc.BackEnd.api.CommonResponse;
 import com.soc.BackEnd.api.SingleResponse;
+import com.soc.BackEnd.config.security.CustomUserDetails;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+
 
 @Api(tags = {"Account API"})
 @RestController
@@ -43,9 +41,8 @@ public class AccountController {
     })
     @ApiOperation(value = "로그인한 회원 조회", notes = "학번(studentId)으로 회원을 조회한다")
     @GetMapping(value = "/get-account")
-    public SingleResponse<ResponseAccountDto> getAccount() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String studentId = authentication.getName();
+    public SingleResponse<ResponseAccountDto> getAccount(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        String studentId = customUserDetails.getUsername();
         return accountService.getAccount(studentId);
     }
 
@@ -54,9 +51,9 @@ public class AccountController {
     })
     @ApiOperation(value = "비밀번호 변경 API", notes ="이전 비밀번호, 새로운 비밀번호 전송")
     @PutMapping(value = "/account/password")
-    public SingleResponse<String> changePwd(@ApiParam(value = "비밀번호 변경 객체", required = true) @RequestBody @Valid PasswordUpdateDto dto, Errors errors){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String studentId = authentication.getName();
+    public SingleResponse<String> changePwd(@ApiParam(value = "비밀번호 변경 객체", required = true) @RequestBody @Valid PasswordUpdateDto dto, Errors errors,
+                                            @AuthenticationPrincipal CustomUserDetails customUserDetails){
+        String studentId = customUserDetails.getUsername();
         return accountService.changePwd(dto, errors, studentId);
     }
 
@@ -65,10 +62,9 @@ public class AccountController {
     })
     @ApiOperation(value = "닉네임 변경 API", notes ="새로운 닉네임 전송")
     @PutMapping(value = "/account-update")
-    public SingleResponse<String> changeNickname(@ApiParam(value = "변경 될 닉네임", required = true)
-                                                     @RequestParam String updateNickname){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String studentId = authentication.getName();
+    public SingleResponse<String> changeNickname(@ApiParam(value = "변경 될 닉네임", required = true) @RequestParam String updateNickname,
+                                                 @AuthenticationPrincipal CustomUserDetails customUserDetails){
+        String studentId = customUserDetails.getUsername();
         return accountService.changeNickname(updateNickname, studentId);
     }
 
