4e233fddb99aee9181369661fe21e75ceb01d3fe
vividswan <vividswan@naver.com>
2021-08-02 11:18:27 +0900

    feat: Post Controller, Service 생성

--- Back-End/src/main/java/com/soc/BackEnd/board/repository/PostRepository.java
+++ Back-End/src/main/java/com/soc/BackEnd/board/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.soc.backend.board.repository;
 
+import com.soc.backend.board.dto.GetPostsPageRes;
 import com.soc.backend.board.entity.Board;
 import com.soc.backend.board.entity.Post;
 import org.springframework.data.domain.Page;
@@ -9,5 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    Page<Post> findAllByBoard(Pageable pageable, Board board);
+    Page<GetPostsPageRes> findAllByBoard(Pageable pageable, Board board);
 }
