686329cfabb7e17320f44c4a5e25e54a78860454
vividswan <vividswan@naver.com>
2021-08-07 16:25:53 +0900

    refactor: Dto Class 이름 변경

--- Back-End/src/main/java/com/soc/BackEnd/account/dto/SignupReq.java
+++ Back-End/src/main/java/com/soc/BackEnd/account/dto/SignupReq.java
@@ -1,11 +1,9 @@
 package com.soc.backend.account.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
@@ -13,14 +11,22 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class LoginDto {
+public class SignupReq {
     @NotBlank
     @Length(min=9,max=9)
     @Pattern(regexp = "^[0-9]{3,20}$")
     private String studentId;
 
     @NotBlank
+    @Length(min=3, max = 20)
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9_-]{3,20}$")
+    private String nickname;
+
+    @NotBlank
     @Length(min=8, max= 50)
     private String password;
 
+    @Email
+    @NotBlank
+    private String email;
 }
