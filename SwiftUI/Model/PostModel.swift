061cdaa68c40af6058132b55853c131d64104e30
Genesis2010 <dmsgktn1685@naver.com>
2021-08-20 21:55:05 +0900

    feat: 게시글 json 형식 구조 추가

--- SwiftUI/Model/PostModel.swift
+++ SwiftUI/Model/PostModel.swift
@@ -7,18 +7,51 @@
 
 import SwiftUI
 
-struct PostModel: Identifiable {
-    
-//    var id = UUID().uuidString
-//    var contents: String
-//    var timing: Data
-//    var comment_cnt: Int
-    
-    var id: String
-    var contents: String
-    var timing: String
-    var comment_cnt: String
+// 고유 ID, 닉네임 올린시간, 댓글 수, 글 내용,
+struct PostJson: Codable {
+    var code: Int
+    var isSuccess : Bool
+    var message : String
+    var result : PostResult
+}
+
+struct PostResult: Codable {
+    var content : [PostContent]
+    var empty : Bool
+    var first : Bool
+    var last : Bool
+    var number : Int
+    var numberOfElements : Int
+    var pageable : PageableResult
+    var size : Int
+    var sort : SortResult
+    var totalElements : Int
+    var totalPages : Int
     
 }
 
-// 닉네임, 글 내용, 올린시간, 댓글 수
+struct PostContent: Codable, Identifiable{
+    let id: UUID = UUID()
+    var postId : Int
+    var status : String
+    var title: String
+    var contents: String
+    var author : String
+    var reportCount : Int
+}
+
+
+struct PageableResult: Codable {
+    var offset : Int
+    var pageNumber : Int
+    var pageSize : Int
+    var paged : Bool
+    var sort : SortResult
+    var unpaged : Bool
+}
+
+struct SortResult: Codable {
+    var empty : Bool
+    var sorted : Bool
+    var unsorted : Bool
+}
