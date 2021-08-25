426e534a18c6a233a3eb13cc644205c3aa0bd26a
Genesis2010 <dmsgktn1685@naver.com>
2021-08-25 20:14:56 +0900

    feat: 게시글 생성 기능 추가

--- SwiftUI/ViewModel/PostViewModel.swift
+++ SwiftUI/ViewModel/PostViewModel.swift
@@ -14,10 +14,21 @@ class PostViewModel: ObservableObject {
 
     @Published var boardNumber = "19"
     @Published var postings = [PostContent]()
+    @Published var TabButtonName = ""
+    
+    @Published var boardId = 19
+    @Published var firstImageUrl = ""
+    @Published var secondImageUrl = ""
+    @Published var contents = ""
+    @Published var title = "Testing"
+    
+    @Published var posting = false
+    @Environment (\.presentationMode) var presentationMode
+    
 
 
     //MARK: - 게시글 조회하는 함수
-    func boardsPosting(completion: @escaping ([PostContent]) -> ()) {
+    func searchPosting(completion: @escaping ([PostContent]) -> ()) {
 
         let url = "https://prod.soc-project.site/api/posts/boards/"
 
@@ -32,11 +43,20 @@ class PostViewModel: ObservableObject {
                     do {
 
                         let json = try JSONSerialization.data(withJSONObject: jsonData, options: .prettyPrinted)
-                        let PostingInData = try JSONDecoder().decode(PostJson.self, from: json)
+                        print("입력2")
+                        
+                        let PostingInData = try! JSONDecoder().decode(PostJson.self, from: json)
 
                         DispatchQueue.main.async {
                             completion(PostingInData.result.content)
                         }
+                        
+//                        let PostingInData = try JSONDecoder().decode(PostJson.self, from: json)
+//                        print("입력1")
+//                        DispatchQueue.main.async {
+//                            print("입력")
+//                            completion(PostingInData.result.content)
+//                        }
                         
                     } catch (let err) {
 
@@ -49,5 +69,52 @@ class PostViewModel: ObservableObject {
                     debugPrint(values)
                 }
             })
+    }
+    
+    //MARK: - 게시글 생성하는 함수
+    func createPosting () -> Bool {
+
+        let url = "https://prod.soc-project.site/api/posts"
+
+        let tk = TokenUtils()
+
+        let parameters: Parameters = [
+            "boardId": boardId as Any,
+            "contents": contents,
+            "firstImageUrl": firstImageUrl,
+            "secondImageUrl": secondImageUrl,
+            "title": title
+        ]
+
+        if let accessToken = tk.load("com.SOC", account: "accessToken") {
+
+            AF.request(url, method: .post, parameters: parameters, encoding: JSONEncoding.default, headers: ["X-ACCESS-TOKEN":accessToken])
+                .responseJSON(completionHandler: { response in
+                    switch response.result {
+                    case .success(let value):
+                        print("서버 연결 success")
+//                        debugPrint(value)
+                        do {
+                            let json = try JSONSerialization.data(withJSONObject: value, options: .prettyPrinted)
+                            let CreatePostingData = try JSONDecoder().decode(PostingResult.self, from: json)
+
+                            if CreatePostingData.code == 1000 {
+                                
+                                
+
+                                self.posting = true
+                                
+                            }
+                        } catch (let err){
+                            print(err.localizedDescription)
+                        }
+
+                    case .failure:
+                        print("failure")
+                    }
+                })
+
+        }
+        return posting
     }
 }
