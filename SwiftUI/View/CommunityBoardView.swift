74cc0a7e5f33abb50349c1fece1cae7fdee1a781
Genesis2010 <dmsgktn1685@naver.com>
2021-08-27 23:16:49 +0900

    feat: 게시글 내용 refresh 동작 추가

--- SwiftUI/View/CommunityBoardView.swift
+++ SwiftUI/View/CommunityBoardView.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
-struct Community_board: View {
+struct CommunityBoardView: View {
+    
+    // sample data
+    @State var arryData = [" Hello Data 1", "Hello Data 2"]
+    @State var refresh = Refresh(started: false, released: false)
     
     @State var posts: [PostContent] = []
     @Namespace var animationID
@@ -21,24 +25,69 @@ struct Community_board: View {
         ZStack(alignment: .bottomTrailing){
             
             NavigationView{
-                VStack{
+                VStack(){
                     TopBarView(title: "커뮤니티")
                     
+                    Divider()
+                    
                     ScrollView(.vertical, showsIndicators: false) {
-                        LazyVStack{
-                            ForEach(posts){ post in
-                                NavigationLink(destination: PasswordFindView()){
-                                    PostingCardView(posting: post)
-                                        .accentColor(.black)
+                        
+                        GeometryReader{reader -> AnyView in
+                            
+//                            print(reader.frame(in: .global).minY) Y 방향의 크기를 구하는 방식
+                            
+                            DispatchQueue.main.async {
+                                if refresh.startOffset == 0 {
+                                    refresh.startOffset = reader.frame(in: .global).minY
                                 }
-                                Divider()
+                                
+                                refresh.offset = reader.frame(in: .global).minY
+                                
+                                if refresh.offset - refresh.startOffset > 80 && !refresh.started {
+                                    refresh.started = true
+                                }
+                                
+                                if refresh.startOffset == refresh.offset && refresh.started && !refresh.released{
+                                    
+                                    refresh.released = true
+                                    
+                                    viewModel.searchPosting() { (posts) in
+                                        self.posts = posts
+                                    }
+                                }
                             }
+                            
+                            return AnyView(Color.black.frame(width:0, height: 0))
                         }
-                        .padding()
+                        .frame(width:0, height: 0)
+                        
+                        ZStack(alignment: Alignment(horizontal: .center, vertical: .top)){
+                            
+                            Image(systemName: "arrow.down")
+                                .font(.system(size: 16, weight: .heavy))
+                                .foregroundColor(.gray)
+                                .rotationEffect(.init(degrees: refresh.started ? 180 : 0))
+                                .offset(y: -25)
+                                .animation(.easeIn)
+                            
+                            LazyVStack{
+                                ForEach(posts){ post in
+                                    NavigationLink(destination: PasswordFindView()){
+                                        PostingCardView(posting: post)
+                                            .accentColor(.black)
+                                            .onAppear()
+                                    }
+                                    Divider()
+                                }
+                            }
+                            .padding()
+                        }
+                        .offset(y: refresh.released ? 40 : -10 )
+                        
                     }
                     .navigationBarHidden(true)
                     .onAppear {
-                        viewModel.boardsPosting() { (posts) in
+                        viewModel.searchPosting() { (posts) in
                             self.posts = posts
                         }
                     }
@@ -50,8 +99,15 @@ struct Community_board: View {
     }
 }
 
-struct Community_board_Previews: PreviewProvider {
+struct CommunityBoardView_Previews: PreviewProvider {
     static var previews: some View {
-        Community_board()
+        CommunityBoardView()
     }
+}
+
+struct Refresh {
+    var startOffset : CGFloat = 0
+    var offset : CGFloat = 0
+    var started : Bool
+    var released : Bool
 }
