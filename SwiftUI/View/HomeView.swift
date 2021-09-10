5c46d47b771cd3e5ed5717e196cc2ec2d0961fa3
Genesis2010 <dmsgktn1685@naver.com>
2021-09-11 00:49:51 +0900

    feat: 홈 화면 구성

---
+++ SwiftUI/View/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  SOCprojectUI
+//
+//  Created by 박준혁 on 2021/02/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        VStack{
+            HStack(alignment: .bottom){
+                Text("SOC")
+                    .font(.title.bold())
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Button {
+                    
+                } label: { // 게시판 종류 바꾸는 버튼
+                    Text("ADD")
+                        .foregroundColor(.black)
+                        .padding(.vertical, 10)
+                        .padding(.horizontal, 20)
+                        .background(
+                            Capsule()
+                                .stroke(Color.black,lineWidth: 1)
+                        )
+                }
+            }
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
+
