6ec2d543d88491e071123b8664b935e535ee1c92
Genesis2010 <dmsgktn1685@naver.com>
2021-09-12 20:55:27 +0900

    feat: 게시글 무한 조회 기능, 자세한 게시글 조회 기능 추가

--- SwiftUI/ViewModel/PostViewModel.swift
+++ SwiftUI/ViewModel/PostViewModel.swift
@@ -16,23 +16,84 @@ class PostViewModel: ObservableObject {
     @Published var postings = [PostContent]()
     @Published var TabButtonName = ""
     
+    @Published var data = [PostContent]()
+    
+    @Published var page = 1
+    
     @Published var boardId = 19
     @Published var firstImageUrl = ""
     @Published var secondImageUrl = ""
     @Published var contents = ""
     @Published var title = "Testing"
     
+    @Published var postId : Int = 0
+    
     @Published var posting = false
     @Environment (\.presentationMode) var presentationMode
+    
+    @Published var detailResult : DetailResult
+    
+    init() {
+        self.detailResult = DetailResult(accountId: 0, accountNickname: "", boardId: 0, contents: "", firstImageUrl: "", postId: 0, reportCount: 0, secondImageUrl: "", status: "", subjectId: 0, subjectName: "", title: "")
+        updateData(19)
+    }
+
+//    func changeNumber(_ number : String){
+//        boardNumber = number
+//        updateData()
+//    }
+    
+    //MARK: - 게시글 무한 조회하는 함수
+    func updateData(_ number: Int) {
+        let url = "https://prod.soc-project.site/api/posts/boards/\(number)?isAsc=false&page=\(page)&size=5&sortBy=postId"
+
+        let session = URLSession(configuration: .default)
+
+        session.dataTask(with: URL(string: url)!) { (data, _, err) in
+
+            if err != nil{
+
+                print((err?.localizedDescription)!)
+                return
+            }
+
+            do {
+                
+                print(number)
+                
+                let json = try JSONDecoder().decode(PostJson.self, from: data!)
+                
+                let oldData = self.data
+
+                DispatchQueue.main.async {
+
+                    self.data = oldData + json.result.content
+                    
+                    self.page += 1
+                }
+
+
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+            
+        }
+        .resume()
+    }
+
     
 
 
     //MARK: - 게시글 조회하는 함수
     func searchPosting(completion: @escaping ([PostContent]) -> ()) {
 
-        let url = "https://prod.soc-project.site/api/posts/boards/"
+//        let url = "https://prod.soc-project.site/api/posts/boards/"
+//        url + boardNumber
+        
+        let url = "https://prod.soc-project.site/api/posts/boards/\(boardNumber)?isAsc=false&page=\(page)&size=10&sortBy=postId"
 
-        AF.request(url + boardNumber, method: .get, parameters: nil, encoding: URLEncoding.default)
+        AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default)
             .responseJSON(completionHandler: { response in
                 switch response.result {
                 case .success(let jsonData):
@@ -43,21 +104,22 @@ class PostViewModel: ObservableObject {
                     do {
 
                         let json = try JSONSerialization.data(withJSONObject: jsonData, options: .prettyPrinted)
-                        print("입력2")
                         
                         let PostingInData = try! JSONDecoder().decode(PostJson.self, from: json)
 
+                        let oldData = self.postings
+                        
                         DispatchQueue.main.async {
+                            
+                            self.postings = oldData + PostingInData.result.content
+                            
+                            self.page += 1
+                            
                             completion(PostingInData.result.content)
+                            
                         }
                         
-//                        let PostingInData = try JSONDecoder().decode(PostJson.self, from: json)
-//                        print("입력1")
-//                        DispatchQueue.main.async {
-//                            print("입력")
-//                            completion(PostingInData.result.content)
-//                        }
-                        
+                      
                     } catch (let err) {
 
                     }
@@ -116,5 +178,40 @@ class PostViewModel: ObservableObject {
 
         }
         return posting
+    }
+    
+    
+    
+    //MARK: - 게시글 자세히 조회하는 함수
+    func detailPosting(_ num : Int) {
+        let url = "https://prod.soc-project.site/api/posts/\(num)"
+
+        AF.request(url, method: .get, parameters: nil, encoding: URLEncoding.default)
+            .responseJSON(completionHandler: { [self] response in
+                    switch response.result {
+                    case .success(let jsonData):
+                        print("success")
+//                        debugPrint(jsonData)
+
+                        do {
+                            let json = try JSONSerialization.data(withJSONObject: jsonData, options: .prettyPrinted)
+                            print("디테일뷰")
+
+                            let jsonData = try! JSONDecoder().decode(DetailJson.self, from: json)
+                            
+                            self.detailResult = DetailResult(accountId: jsonData.result.accountId, accountNickname: jsonData.result.accountNickname, boardId: jsonData.result.boardId, contents: jsonData.result.contents, firstImageUrl: jsonData.result.firstImageUrl, postId: jsonData.result.postId, reportCount: jsonData.result.reportCount, secondImageUrl: jsonData.result.secondImageUrl, status: jsonData.result.status, subjectId: jsonData.result.subjectId, subjectName: jsonData.result.subjectName, title: jsonData.result.title)
+                            
+                        }
+                        catch (let err) {
+
+                        }
+
+
+                    case .failure(let values):
+                        print("failure")
+
+                        debugPrint(values)
+                    }
+                })
     }
 }
