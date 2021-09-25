4d1ae9376238fdcfd6b693f7b3f670184e07f6ad
Genesis2010 <dmsgktn1685@naver.com>
2021-09-25 20:36:36 +0900

    Fix: 최적화가 되어있는 LazyVStack으로 변경

--- SwiftUI/View/CommunityBoardView.swift
+++ SwiftUI/View/CommunityBoardView.swift
@@ -9,45 +9,91 @@ import SwiftUI
 
 struct CommunityBoardView: View {
     
+    @State var posts: [PostContent] = []
     @ObservedObject var viewModel = PostViewModel()
     
     @State var showModal = false
     
+//    var posting : PostContent
+    
     var body: some View {
         
         NavigationView{
-            List(0..<viewModel.data.count, id: \.self) { i in
-
-                if i == self.viewModel.data.count - 1 {
-                    PostingCardView(posting: self.viewModel.data[i], isLast: true, viewModel: self.viewModel)
-                        .onTapGesture(perform: {
-                            showModal = true
-                            viewModel.detailPosting(viewModel.data[i].postId)
+            ScrollView(.vertical, showsIndicators: false) {
+                LazyVStack{
+                    ForEach(posts){ post in
+                        
+                        NavigationLink(destination: TestDetailView(posting: self.viewModel.detailResult)){
+                            TestCardView(posting: post)
+                                .accentColor(.black)
+                        }
+                        .simultaneousGesture(TapGesture().onEnded{
                             
+                            viewModel.detailPosting(post.postId)
+//                            self.show.toggle()
                         })
-                        .fullScreenCover(isPresented: $showModal, content: {
-                            DetailPageView(posting: self.viewModel.detailResult)
-                        })
+                        
+//                        .onAppear{
+//                            viewModel.detailPosting(post.postId)
+//                        }
+                        
+                        Divider()
+                    }
                 }
-                else {
-                    PostingCardView(posting: self.viewModel.data[i], isLast: false, viewModel: self.viewModel)
-                        .onTapGesture(perform: {
-                            showModal = true
-                            viewModel.detailPosting(viewModel.data[i].postId)
-                        })
-                        .fullScreenCover(isPresented: $showModal, content: {
-                            DetailPageView(posting: self.viewModel.detailResult)
-                        })
-                }
+                .padding()
             }
             .navigationBarHidden(true)
-            
+            .onAppear {
+                DispatchQueue.main.async {
+                    viewModel.searchPosting(19) { (posts) in
+                        self.posts = posts
+                        print(posts.count)
+                    }
+                }
+            }
+        
         }
+        
+        
+        
+        
+        
+//        NavigationView{
+//            List(0..<viewModel.data.count, id: \.self) { i in
+//
+//                if i == self.viewModel.data.count - 1 {
+//                    PostingCardView(posting: self.viewModel.data[i], isLast: true, viewModel: self.viewModel)
+//                        .onTapGesture(perform: {
+//                            showModal = true
+//                            viewModel.detailPosting(viewModel.data[i].postId)
+//
+//                        })
+//                        .fullScreenCover(isPresented: $showModal, content: {
+//                            DetailPageView(posting: self.viewModel.detailResult)
+//                        })
+//                }
+//                else {
+//                    PostingCardView(posting: self.viewModel.data[i], isLast: false, viewModel: self.viewModel)
+//                        .onTapGesture(perform: {
+//                            showModal = true
+//                            viewModel.detailPosting(viewModel.data[i].postId)
+//                        })
+//                        .fullScreenCover(isPresented: $showModal, content: {
+//                            DetailPageView(posting: self.viewModel.detailResult)
+//                        })
+//                }
+//            }
+//            .navigationBarHidden(true)
+//            .onAppear{
+//                viewModel.updateData(19)
+//            }
+//
+//        }
     }
 }
 
 struct CommunityBoardView_Previews: PreviewProvider {
     static var previews: some View {
-        CommunityBoardView()
+        ContentView()
     }
 }
